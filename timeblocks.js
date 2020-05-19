5d3d6c1f350ee4f736130005da83d524e6b16e63
iganymede <57736279+iganymede@users.noreply.github.com>
2020-05-20 01:05:05 +0800

    Update timeblocks.js

--- timeblocks.js
+++ timeblocks.js
@@ -1,11 +1,11 @@
 let body= $response.body;
 var obj = JSON.parse(body);
 if (body.indexOf("/api/mem") !=-1) {
-  obj.premiumEnd = "4096019658000";
-  obj.premium = "1";
+  obj."premiumEnd" = 4096019658000;
+  obj."premium" = 1;
   }
 if (body.indexOf("/api/inbox") !=-1) {
-  obj.premiumEnd = "4096019658000";
-  obj.premium = "1";
+  obj."premiumEnd" = 4096019658000;
+  obj."premium" = 1;
   }
 $done({body: JSON.stringify(obj)});
