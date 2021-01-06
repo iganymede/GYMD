3b3bf75d8c2c3c9e3dbe0df3e312539a0455b27e
iganymede <57736279+iganymede@users.noreply.github.com>
2021-01-06 10:27:36 +0800

    Update fantastical.js

--- fantastical.js
+++ fantastical.js
@@ -1,16 +1,36 @@
-let obj = JSON.parse($response.body);
-obj.subscription = {
-    "autorenew": true,
-    "additional_seats": 4,
-    "expiration": "2099-12-31T16:49:37.000000Z",
-    "uuid": "78hg9ji0-9j80-40kk-nj90-897827klsf6",
-    "subscription_type": "AppStore",
-    "is_expired": false,
-    "is_canceled": false,
-    "trial": true,
-    "dependents": [],
-    "parent": null,
-    "pending_invitations": []
-  };
+const path1="device";
+const path2="details";
+const path3="appstore-receipt";
+let url=$request.url;
+let obj=JSON.parse($response.body);
 
-$done({body: JSON.stringify(obj)});
+if(url.indexOf(path1)!=-1){
+
+  obj.subscription.autorenew=true;
+  obj.subscription.expiration="2099-02-01T16:49:37Z";
+  obj.subscription.expires="2099-02-01T16:49:37Z";
+  obj.scope=["notify", "weather", "keyvalue-watch", "keyvalue-verification", "schedjoules", "scheduling", "account", "keyvalue", "fantastical"];
+
+}
+
+
+if(url.indexOf(path2)!=-1){
+
+  obj.subscription.autorenew=true;
+  obj.subscription.expiration="2099-02-01T16:49:37.000000Z";
+  obj.subscription.subscription_type="AppStore";
+  obj.subscription.is_expired=false;
+  obj.subscription.trial=false;
+    
+}
+
+if(url.indexOf(path3)!=-1){
+	
+	obj.autorenew=true;
+	obj.expiration="2099-02-01T16:49:37.000000Z";
+	obj.subscription_type="AppStore";
+	obj.is_expired=false;
+	obj.trial=false;
+}
+
+$done({body:JSON.stringify(obj)});
